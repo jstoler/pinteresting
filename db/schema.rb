e11ca005faad47a9e0d4f4bde64b53fcabdfb2f0
Josh Stoler <jstoler3131@gmail.com>
2015-11-12 05:35:35 -0500

    Added Scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106163942) do
+ActiveRecord::Schema.define(version: 20151112102349) do
+
+  create_table "pins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
